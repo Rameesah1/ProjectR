b74dfd30738a63ba21cd0b2ab558414b3dd8e229
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2024-03-12 01:01:12 +0000

    unfinished

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # removing read permissions from this file 
-chmod -r data/bad_perms.csv
+chmod -r data/bad_perms.txt
 
 gcc mazeGame.c -o mazeGame
 
@@ -13,13 +13,13 @@ echo -e "Argument tests"
 echo -n "Testing no arguments - "
 ./maze  > tmp
 if grep -q "mazeGame <filename>"" tmp;
- then
+then
     echo "PASS"
 else
     echo "FAIL"
 fi
 
-# excess file arguments entered tested 
+# excess file arguments entered tested
 
 echo -n "Testing two arguments -"t
 ./mazeGame a b > tmp
@@ -31,12 +31,25 @@ else
 fi
 
 echo -e "File tests"
-# testing when the file doesn't not exist in repository 
+ 
+# testing file format
+
+echo -n "Testing invalid file type - "
+timeout 0.2s ./mazeGame data/invalidfiletype.pdf > tmp
+if grep -q "Error : Wrong file format" tmp;
+then
+    echo "PASS"
+else 
+    echo "FAIL"
+fi
+
+# testing for a non existent file in the repository
 
 echo -n "Testing the file exists -"
-./maze hypothetical.txt > out
-if grep -q "Error: File not found" out;
+./mazeGame non_existent_file.txt > tmp
+if grep -q "Error: File not found" tmp;
 then
     echo "PASS"
 else
     echo "FAIL"
+fi
